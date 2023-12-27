63a432226e751ddc603429a97714795d4e874472
Jihunee <jihun6439@daum.net>
2023-12-27 11:14:15 +0900

    🎉 프로필 페이지 생성

---
+++ src/app/profilepage/page.tsx
@@ -0,0 +1,14 @@
+import NavBar from "@/components/NavBar";
+import ProfileInfo from "@/components/ProfileInfo";
+import React from "react";
+
+function ProfilePage() {
+  return (
+    <>
+      <ProfileInfo />
+      <NavBar />
+    </>
+  );
+}
+
+export default ProfilePage;
