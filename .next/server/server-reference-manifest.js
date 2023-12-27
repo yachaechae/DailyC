e243c70cbeca3923b3e9eadca011b23e5c639281
volant97 <volant97@naver.com>
2023-12-27 11:13:00 +0900

    🎉 login 페이지 생성

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ntMiPJISFoSVfNdZ/SCxUBNL7fW8bGF9RwSqfe2L5B0=\"\n}"
---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>페이지입니다</div>;
+}
+
+export default page;
