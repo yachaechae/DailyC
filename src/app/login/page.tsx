5621e9f447d68aa74c30971a658599c6fee9fe5d
volant97 <volant97@naver.com>
2023-12-29 14:57:59 +0900

    ✨ 로그인 화면 개선

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -43,39 +43,66 @@ function page() {
   // 비밀번호 찾기 기능
 
   return (
-    <>
-      <form onSubmit={handleLginForm}>
-        <div>
-          <p>이메일</p>
-          <input type="text" value={email} onChange={handleEmailInput} />
-        </div>
-        <div>
-          <p>비밀번호</p>
+    <div className="container flex justify-center items-center h-[calc(100vh-75px)]">
+      <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col w-full ">
+        <h2 className="text-gray-900 text-lg font-medium title-font mb-5 text-center">
+          로그인
+        </h2>
+        <form onSubmit={handleLginForm}>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+              이메일
+            </label>
+            <input
+              className="w-full bg-white rounded border border-gray-300 focus:border-deepOrange focus:ring-2 focus:ring-lightOrange text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={handleEmailInput}
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="password"
+              className="leading-7 text-sm text-gray-600"
+            >
+              비밀번호
+            </label>
+            <input
+              className="w-full bg-white rounded border border-gray-300 focus:border-deepOrange focus:ring-2 focus:ring-lightOrange text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={handlePasswordInput}
+            />
+          </div>
+          {/* <div className="relative mb-4">
+          <label htmlFor="nickname" className="leading-7 text-sm text-gray-600">
+            닉네임
+          </label>
           <input
-            type="password"
-            value={password}
-            onChange={handlePasswordInput}
+            type="text"
+            id="nickname"
+            name="nickname"
+            className="w-full bg-white rounded border border-gray-300 focus:border-deepOrange focus:ring-2 focus:ring-lightOrange text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
           />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      <div>
-        <p>소셜 로그인</p>
-        <p>-------------</p>
-        <div>
-          <div>이미지1</div>
-          <div>이미지2</div>
-          <div>이미지3</div>
-        </div>
-        <p>-------------</p>
-        <div>
-          <a>비밀번호찾기</a>
+        </div> */}
+          <button
+            className="text-white bg-orange border-0 py-2 px-8 focus:outline-none hover:bg-deepOrange rounded text-lg w-full"
+            type="submit"
+          >
+            로그인
+          </button>
+        </form>
+        <div className="flex justify-end w-full">
           <Link href={"/signup"} legacyBehavior>
-            <a>회원가입</a>
+            <a className="text-xs text-gray-500 mt-3 w-12">회원가입</a>
           </Link>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
