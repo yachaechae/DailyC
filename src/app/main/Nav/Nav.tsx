8a18b8f01b56bc2920f9e05989a3eae992c1b51f
yachaechae <wpql1207@gmail.com>
2023-12-29 16:22:36 +0900

    💄 tailwind css 수정

--- src/app/main/Nav/Nav.tsx
+++ src/app/main/Nav/Nav.tsx
@@ -1,15 +1,13 @@
 import { Judson } from "next/font/google";
 import React from "react";
-import styles from "./nav.module.css";
-import { Container } from "../main.styles";
-import { Category, CategoryItem, NavSt } from "./st-nav";
+import "../custom.styles.css";
 
 const judson = Judson({ weight: "400", subsets: ["latin"] });
 export default function Nav() {
 	return (
 		<nav className={`${judson.className} border-b border-solid border-b-gray-300`}>
 			<div className="container xl w-full flex justify-between items-center">
-				<ul className="w-full h-75 text-3xl flex items-center justify-center gap-12">
+				<ul className="w-full h-[75] text-3xl flex items-center justify-center gap-12 last:after:bg-transparent">
 					<li className="custom-category-item">ALL</li>
 					<li className="custom-category-item">WOMEN</li>
 					<li className="custom-category-item">MEN</li>
