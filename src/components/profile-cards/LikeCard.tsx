721ee7b4382d97013d17ffa615e2692bf307fc80
Jihunee <jihun6439@daum.net>
2023-12-30 19:23:29 +0900

    🎉 프로필 페이지 / 카드 호버 효과 추가

--- src/components/profile-cards/LikeCard.tsx
+++ src/components/profile-cards/LikeCard.tsx
@@ -23,17 +23,17 @@ export default function LikeCard() {
   };
   return (
     <Card className="rounded-[15px]" sx={{ maxWidth: 250 }}>
-      <div className="relative">
+      <div className="relative group">
         <CardMedia
           component="img"
           image="http://placekitten.com/200/300"
         ></CardMedia>
-        <div className="absolute w-full bottom-0 left-0 bg-slate-50 bg-opacity-50 p-4">
+        <div className="absolute w-full bottom-0 left-0 bg-slate-50 bg-opacity-50 p-4 hidden group-hover:block transition duration-200 ease-out group-hover:ease-in">
           <Typography
             variant="h4"
-            className={`${jua.className} text-white drop-shadow-xl`}
+            className={`${jua.className} text-white drop-shadow-[0_1px_5px_rgba(0,0,0,0.7)] truncate`}
           >
-            편안 데일리룩
+            편안 데일리룩 편안 데일리룩
           </Typography>
           <Typography variant="h6" className={`${jua.className}`}>
             #트레이닝 # 편안 #집앞
