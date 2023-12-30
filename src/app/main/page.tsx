9a16fa46abf24ff2e1d84ac104a6a72702f9b57f
volant97 <volant97@naver.com>
2023-12-30 19:22:42 +0900

    🐛 경로 오류 수정

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,22 +1,19 @@
 "use client";
 import React from "react";
-import Banner from "../../components/main/Banner/Banner";
-import Nav from "../../components/main/Nav/Nav";
-import Popular from "../../components/main/Popular/Popular";
-import Recent from "../../components/main/Recent/Recent";
-import { Judson } from "next/font/google";
-
-const judson = Judson({ weight: "400", subsets: ["latin"] });
+import Banner from "@/components/main/banner/Banner";
+import Nav from "@/components/main/nav/Nav";
+import Popular from "@/components/main/popular/Popular";
+import Recent from "@/components/main/recent/Recent";
 
 export default function page() {
-	return (
-		<>
-			<Nav />
-			<Banner />
-			<div className="container mt-[10rem]">
-				<Popular />
-				<Recent />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Nav />
+      <Banner />
+      <div className="container mt-[10rem]">
+        <Popular />
+        <Recent />
+      </div>
+    </>
+  );
 }
--- src/components/main/nav/Nav.tsx
+++ src/components/main/nav/Nav.tsx
@@ -1,18 +1,20 @@
 import { Judson } from "next/font/google";
 import React from "react";
-import "../custom.styles.css";
+import "@/app/main/custom.styles.css";
 
 const judson = Judson({ weight: "400", subsets: ["latin"] });
 export default function Nav() {
-	return (
-		<nav className={`${judson.className} border-b border-solid border-b-gray-300`}>
-			<div className="container xl w-full flex justify-between items-center">
-				<ul className="w-full h-[75px] text-3xl flex items-center justify-center gap-12 last:after:bg-transparent">
-					<li className="custom-category-item">ALL</li>
-					<li className="custom-category-item">WOMEN</li>
-					<li className="custom-category-item">MEN</li>
-				</ul>
-			</div>
-		</nav>
-	);
+  return (
+    <nav
+      className={`${judson.className} border-b border-solid border-b-gray-300`}
+    >
+      <div className="container xl w-full flex justify-between items-center">
+        <ul className="w-full h-[75px] text-3xl flex items-center justify-center gap-12 last:after:bg-transparent">
+          <li className="custom-category-item">ALL</li>
+          <li className="custom-category-item">WOMEN</li>
+          <li className="custom-category-item">MEN</li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
