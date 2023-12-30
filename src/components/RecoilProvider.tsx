c750854ccfdf8bc9e03c8af34c4c98741c378b06
yachaechae <wpql1207@gmail.com>
2023-12-30 21:06:00 +0900

    ✨mui 테마 설정

--- src/components/RecoilProvider.tsx
+++ src/components/RecoilProvider.tsx
@@ -1,7 +1,13 @@
 "use client";
+import theme from "@/style/customTheme";
+import { ThemeProvider } from "@mui/material";
 import React from "react";
 import { RecoilRoot } from "recoil";
 
 export default function RecoilProvider({ children }: { children: React.ReactNode }) {
-	return <RecoilRoot>{children}</RecoilRoot>;
+	return (
+		<RecoilRoot>
+			<ThemeProvider theme={theme}>{children}</ThemeProvider>
+		</RecoilRoot>
+	);
 }
