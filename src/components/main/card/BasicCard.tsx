3e8b97c1832d8856ac00f007959513c3d8582bdc
yachaechae <wpql1207@gmail.com>
2023-12-31 00:52:26 +0900

    ✨랭킹 아이콘 추가

--- src/components/main/card/BasicCard.tsx
+++ src/components/main/card/BasicCard.tsx
@@ -10,11 +10,13 @@ import Typography from "@mui/material/Typography";
 import OrangeIcon from "@/icon/OrangeIcon";
 import { AccountCircle, Bookmark, BookmarkBorder, WorkspacePremium } from "@mui/icons-material";
 import { Jua, Judson } from "next/font/google";
+import { brown, grey, yellow } from "@mui/material/colors";
+import UserImg from "@/components/profile/UserImg";
 
 const jua = Jua({ weight: "400", subsets: ["latin"] });
 const judson = Judson({ weight: "400", subsets: ["latin"] });
 
-export default function BasicCard() {
+export default function BasicCard({ rank }: { rank: string }) {
 	const [liked, setLiked] = useState(false);
 	const [bookmark, setBookmark] = useState(false);
 
@@ -24,39 +26,43 @@ export default function BasicCard() {
 	const handleBookmarkClick = () => {
 		setBookmark(!bookmark);
 	};
+	const colorData: { [key: string]: string } = {
+		first: yellow["A400"],
+		second: grey[300],
+		third: brown["A400"],
+	};
+
 	return (
 		<Card className="w-[250px]">
-			<div className="relative group">
-				<div className="absolute top-1 w-full flex justify-between">
-					<WorkspacePremium className="size-12 fill-gold drop-shadow" />
-					<div onClick={handleBookmarkClick} className="cursor-pointer">
+			<div className={`${jua.className} relative group `}>
+				<div className="absolute top-1 w-full flex justify-between flex-row-reverse">
+					<div onClick={handleBookmarkClick} className="cursor-pointer ">
 						{bookmark ? (
-							<Bookmark className="size-12 fill-orange " />
+							<Bookmark color="primary" sx={{ fontSize: 48 }} />
 						) : (
-							<BookmarkBorder className="size-12" />
+							<BookmarkBorder sx={{ fontSize: 48 }} />
 						)}
 					</div>
+					{!rank ? (
+						<></>
+					) : (
+						<WorkspacePremium
+							className="drop-shadow-[0_1px_1px_rgba(0,0,0,0.8)]"
+							sx={{ color: colorData[rank], fontSize: 48 }}
+						/>
+					)}
 				</div>
 				<CardMedia component="img" image="http://placekitten.com/200/300"></CardMedia>
 				<div className="absolute w-full bottom-0 left-0 bg-slate-50 bg-opacity-50 p-4 hidden group-hover:block transition duration-200 ease-out group-hover:ease-in">
-					<Typography
-						variant="h4"
-						className={`${jua.className} text-white drop-shadow-[0_1px_5px_rgba(0,0,0,0.7)] truncate`}
-					>
+					<p className={` text-3xl text-white drop-shadow-[0_1px_5px_rgba(0,0,0,0.7)] truncate`}>
 						편안 데일리룩 편안 데일리룩
-					</Typography>
-					<Typography variant="h6" className={`${jua.className}`}>
-						#트레이닝 # 편안 #집앞
-					</Typography>
+					</p>
+					<span className={` text-lg`}>#트레이닝 # 편안 #집앞</span>
 				</div>
 			</div>
 			<CardHeader
 				className="bg-gray-300 py-2 px-3"
-				avatar={
-					<Avatar sx={{ bgcolor: "white" }}>
-						<AccountCircle className="fill-orange size-[125%]"> </AccountCircle>
-					</Avatar>
-				}
+				avatar={<UserImg />}
 				action={
 					<IconButton
 						aria-label="add to favorites"
@@ -66,8 +72,8 @@ export default function BasicCard() {
 						<OrangeIcon width={35} liked={liked}></OrangeIcon>
 					</IconButton>
 				}
-				title={<Typography className={`${judson.className}`}>유저이름</Typography>}
-				subheader={<Typography className={`${judson.className}`}>180cm</Typography>}
+				title={<div className={`${judson.className}`}>유저이름</div>}
+				subheader={<div className={`${judson.className}`}>180cm</div>}
 			/>
 		</Card>
 	);
