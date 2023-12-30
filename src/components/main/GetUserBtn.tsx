40314a7af882901627cca75ac8235d79cdf93c81
yachaechae <wpql1207@gmail.com>
2023-12-30 21:06:27 +0900

    🩹mui 아이콘 테마 기준으로 바뀌게 변경 완료

--- src/components/main/GetUserBtn.tsx
+++ src/components/main/GetUserBtn.tsx
@@ -4,18 +4,18 @@ import React from "react";
 import UserImg from "../profile/UserImg";
 
 function GetUserBtn() {
-  const handleGetUserBtn = async () => {
-    const profile = await getUser();
-    if (!profile) return alert("로그인된 이메일이 없습니다.");
-    console.log("user 정보 ", profile);
-  };
+	const handleGetUserBtn = async () => {
+		const profile = await getUser();
+		if (!profile) return alert("로그인된 이메일이 없습니다.");
+		console.log("user 정보 ", profile);
+	};
 
-  return (
-    <>
-      <button onClick={handleGetUserBtn}>유저정보</button>
-      <UserImg />
-    </>
-  );
+	return (
+		<>
+			<button onClick={handleGetUserBtn}>유저정보</button>
+			<UserImg />
+		</>
+	);
 }
 
 export default GetUserBtn;
--- src/components/profile/UserImg.tsx
+++ src/components/profile/UserImg.tsx
@@ -6,35 +6,36 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 function UserImg() {
-  const [profile, setProfile] = useState<any>({});
-  const userImg = profile?.user_metadata?.userImg;
+	const [profile, setProfile] = useState<any>({});
+	const userImg = profile?.user_metadata?.userImg;
 
-  const getProfile = async () => {
-    const data = await getUser();
-    setProfile(data);
-  };
+	const getProfile = async () => {
+		const data = await getUser();
+		setProfile(data);
+	};
 
-  useEffect(() => {
-    getProfile();
-  }, []);
+	useEffect(() => {
+		getProfile();
+	}, []);
 
-  console.log("userImg : ", userImg);
-  console.log("정상인가 : ", !userImg);
-
-  return (
-    <>
-      {!userImg ? (
-        <Avatar sx={{ bgcolor: "white" }}>
-          <AccountCircle className="fill-orange size-[125%]"> </AccountCircle>
-        </Avatar>
-      ) : (
-        <Image src={userImg} alt="테스트" width={120} height={120} />
-      )}
-      <Avatar sx={{ bgcolor: "white" }}>
-        <AccountCircle className="fill-orange size-[125%]"> </AccountCircle>
-      </Avatar>
-    </>
-  );
+	return (
+		<>
+			{!userImg ? (
+				<Avatar sx={{ bgcolor: "white" }}>
+					<AccountCircle color="primary" sx={{ fontSize: 50 }}>
+						{" "}
+					</AccountCircle>
+				</Avatar>
+			) : (
+				<Image src={userImg} alt="테스트" width={120} height={120} />
+			)}
+			<Avatar sx={{ bgcolor: "white" }}>
+				<AccountCircle color="primary" sx={{ fontSize: 50 }}>
+					{" "}
+				</AccountCircle>
+			</Avatar>
+		</>
+	);
 }
 
 export default UserImg;
