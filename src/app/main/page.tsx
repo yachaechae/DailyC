dca8108f82d7a5e1a888e451bddbd7f74cee7645
yachaechae <wpql1207@gmail.com>
2023-12-30 19:05:33 +0900

    🚚 컴포넌트 파일 -> 컴포넌트 폴더로 이동

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import React from "react";
-import Banner from "./Banner/Banner";
-import Nav from "./Nav/Nav";
-import Popular from "./Popular/Popular";
-import Recent from "./Recent/Recent";
+import Banner from "../../components/main/Banner/Banner";
+import Nav from "../../components/main/Nav/Nav";
+import Popular from "../../components/main/Popular/Popular";
+import Recent from "../../components/main/Recent/Recent";
 import { Judson } from "next/font/google";
 
 const judson = Judson({ weight: "400", subsets: ["latin"] });
--- src/components/main/popular/Popular.tsx
+++ src/components/main/popular/Popular.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import BasicCard from "../Card/BasicCard";
-import { Title } from "../main.styles";
+import { Title } from "../../../app/main/main.styles";
 
 export default function Popular() {
 	return (
