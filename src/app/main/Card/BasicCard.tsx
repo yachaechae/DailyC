7df8c4d11950af6bb598881793c958db95d3c67c
yachaechae <wpql1207@gmail.com>
2023-12-30 17:34:32 +0900

    💄 BasicCard text-overflow 효과 적용

--- src/app/main/Card/BasicCard.tsx
+++ src/app/main/Card/BasicCard.tsx
@@ -28,9 +28,9 @@ export default function BasicCard() {
 				<div className="absolute w-full bottom-0 left-0 bg-slate-50 bg-opacity-50 p-4 hidden group-hover:block transition duration-200 ease-out group-hover:ease-in">
 					<Typography
 						variant="h4"
-						className={`${jua.className} text-white drop-shadow-[0_1px_5px_rgba(0,0,0,0.7)]`}
+						className={`${jua.className} text-white drop-shadow-[0_1px_5px_rgba(0,0,0,0.7)] truncate`}
 					>
-						편안 데일리룩
+						편안 데일리룩 편안 데일리룩
 					</Typography>
 					<Typography variant="h6" className={`${jua.className}`}>
 						#트레이닝 # 편안 #집앞
