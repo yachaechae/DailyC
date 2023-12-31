45ffd441184c0ab3bcef0a247b674faf3662f9c6
yachaechae <wpql1207@gmail.com>
2024-01-01 05:23:09 +0900

    ✨ getAllPosts 부분 타입 지정 완료 및 에러 핸들링 추가

--- src/api/write.ts
+++ src/api/write.ts
@@ -1,22 +1,25 @@
 import { supabase } from "@/lib/supabase-config";
 
-export async function getAllPosts() {
-  const { data: posts, error } = await supabase.from("posts").select();
+export async function getAllPosts(): Promise<postType[]> {
+	const { data: posts, error } = await supabase.from("posts").select().returns<postType[]>();
 
-  if (error) console.log("Error creating a posts data", error);
-  return posts;
+	if (error || null) {
+		console.log("Error creating a posts data", error);
+		throw new Error("error while fetching posts data");
+	}
+	return posts;
 }
 
 export async function getEventByPost(item: string, id: string) {
-  const { data, error } = await supabase.from("posts").select().eq(item, id);
+	const { data, error } = await supabase.from("posts").select().eq(item, id);
 
-  if (error) console.log("Error creating a posts data", error);
-  return data;
+	if (error) console.log("Error creating a posts data", error);
+	return data;
 }
 
 export async function getEventByPostDelete(item: string, id: string) {
-  const { data, error } = await supabase.from("posts").delete().eq(item, id);
+	const { data, error } = await supabase.from("posts").delete().eq(item, id);
 
-  if (error) console.log("Error delete a posts data", error);
-  else return "성공";
+	if (error) console.log("Error delete a posts data", error);
+	else return "성공";
 }
--- src/components/main/popular/Popular.tsx
+++ src/components/main/popular/Popular.tsx
@@ -1,9 +1,30 @@
+"use client";
 import React from "react";
 import BasicCard from "../card/BasicCard";
 import { ArrowBackIosRounded, ArrowForwardIosRounded } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
+import { getAllPosts } from "@/api/write";
 
-export default function Popular() {
+export default async function Popular() {
+	console.log("render");
+	try {
+		const postData: postType[] = await getAllPosts();
+		console.log(postData);
+	} catch (error) {
+		return (
+			<>
+				<div>게시글을 불러오던 중 에러가 발생했습니다</div>
+			</>
+		);
+	}
+	// useEffect(() => {
+	// 	const fetchData = getAllPosts();
+	// 	fetchData.then((data ) => {
+	// 		data === null;
+	// 		postData = data;
+	// 	});
+	// }, []);
+
 	return (
 		<>
 			<h2 className="mb-14 text-4xl">인기 코디</h2>
