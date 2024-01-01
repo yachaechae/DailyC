8134453190d8a492f247ed0b5874a288ffef7432
yachaechae <wpql1207@gmail.com>
2024-01-02 02:58:04 +0900

    ✨ 카드 클릭 시 해당 페이지 이동 연결 완료

--- src/components/main/card/BasicCard.tsx
+++ src/components/main/card/BasicCard.tsx
@@ -11,6 +11,7 @@ import { Bookmark, BookmarkBorder, WorkspacePremium } from "@mui/icons-material"
 import { Jua, Judson } from "next/font/google";
 import { brown, grey, yellow } from "@mui/material/colors";
 import UserImg from "@/components/profile/UserImg";
+import Link from "next/link";
 
 const jua = Jua({ weight: "400", subsets: ["latin"] });
 const judson = Judson({ weight: "400", subsets: ["latin"] });
@@ -19,63 +20,67 @@ export default function BasicCard({ rank, data }: { rank?: string; data: postTyp
 	const [liked, setLiked] = useState(false);
 	const [bookmark, setBookmark] = useState(false);
 
-  const handleLikeClick = () => {
-    setLiked(!liked);
-  };
-  const handleBookmarkClick = () => {
-    setBookmark(!bookmark);
-  };
-  const colorData: { [key: string]: string } = {
-    first: yellow["A400"],
-    second: grey[300],
-    third: brown["A400"],
-  };
+	const handleLikeClick = () => {
+		setLiked(!liked);
+	};
+	const handleBookmarkClick = () => {
+		setBookmark(!bookmark);
+	};
+	const colorData: { [key: string]: string } = {
+		first: yellow["A400"],
+		second: grey[300],
+		third: brown["A400"],
+	};
 
 	return (
-		<Card className="w-[250px]">
-			<div className={`${jua.className} relative group `}>
-				<div className="absolute top-1 w-full flex justify-between flex-row-reverse">
-					<div onClick={handleBookmarkClick} className="cursor-pointer ">
-						{bookmark ? (
-							<Bookmark color="primary" sx={{ fontSize: 48 }} />
+		<Link href={`/posts/${data.id}`}>
+			<Card className="w-[250px]">
+				<div className={`${jua.className} relative group `}>
+					<div className="absolute top-1 w-full flex justify-between flex-row-reverse">
+						<div onClick={handleBookmarkClick} className="cursor-pointer ">
+							{bookmark ? (
+								<Bookmark color="primary" sx={{ fontSize: 48 }} />
+							) : (
+								<BookmarkBorder sx={{ fontSize: 48 }} />
+							)}
+						</div>
+						{!rank ? (
+							<></>
 						) : (
-							<BookmarkBorder sx={{ fontSize: 48 }} />
+							<WorkspacePremium
+								className="drop-shadow-[0_1px_1px_rgba(0,0,0,0.8)]"
+								sx={{ color: colorData[rank], fontSize: 48 }}
+							/>
 						)}
 					</div>
-					{!rank ? (
-						<></>
-					) : (
-						<WorkspacePremium
-							className="drop-shadow-[0_1px_1px_rgba(0,0,0,0.8)]"
-							sx={{ color: colorData[rank], fontSize: 48 }}
-						/>
-					)}
+					<CardMedia component="img" image={data.mainImg} className="min-h-[310px]"></CardMedia>
+					<div className="absolute w-full bottom-0 left-0 bg-slate-50 bg-opacity-50 p-4 hidden group-hover:block transition duration-200 ease-out group-hover:ease-in">
+						<p className={` text-3xl text-white drop-shadow-[0_1px_5px_rgba(0,0,0,0.7)] truncate`}>
+							{data.title}
+						</p>
+						<span className={` text-lg`}>{data.tags?.join("# ")}</span>
+					</div>
 				</div>
-				<CardMedia component="img" image={data.mainImg} className="min-h-[310px]"></CardMedia>
-				<div className="absolute w-full bottom-0 left-0 bg-slate-50 bg-opacity-50 p-4 hidden group-hover:block transition duration-200 ease-out group-hover:ease-in">
-					<p className={` text-3xl text-white drop-shadow-[0_1px_5px_rgba(0,0,0,0.7)] truncate`}>
-						{data.title}
-					</p>
-					<span className={` text-lg`}>{data.tags?.join("# ")}</span>
-				</div>
-			</div>
-			<CardHeader
-				className="bg-gray-300 py-2 px-2 h-fit"
-				avatar={<div className="h-[35px]">
-				<UserImg size={35} />
-			  </div>}
-				action={
-					<IconButton
-						aria-label="add to favorites"
-						onClick={handleLikeClick}
-						className={liked ? "liked" : ""}
-					>
-						<OrangeIcon width={35} liked={liked}></OrangeIcon>
-					</IconButton>
-				}
-				title={<div className={`${judson.className} max-w-120px w-full`}>{data.writedName}</div>}
-				subheader={<div className={`${judson.className}`}>{data.height}</div>}
-			/>
-		</Card>
+				<CardHeader
+					className="bg-gray-300 py-2 px-2 h-fit"
+					avatar={
+						<div className="h-[35px]">
+							<UserImg size={35} />
+						</div>
+					}
+					action={
+						<IconButton
+							aria-label="add to favorites"
+							onClick={handleLikeClick}
+							className={liked ? "liked" : ""}
+						>
+							<OrangeIcon width={35} liked={liked}></OrangeIcon>
+						</IconButton>
+					}
+					title={<div className={`${judson.className} max-w-120px w-full`}>{data.writedName}</div>}
+					subheader={<div className={`${judson.className}`}>{data.height}</div>}
+				/>
+			</Card>
+		</Link>
 	);
 }
