2fb08ef49a14f7f7a2735822deb09bd68fb0def9
yachaechae <wpql1207@gmail.com>
2024-01-03 01:58:42 +0900

    ✨푸터 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,49 +11,40 @@ const jua = Jua({ weight: "400", subsets: ["latin"] });
 const judson = Judson({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "DailyC",
-  description: "오늘의 코디",
+	title: "DailyC",
+	description: "오늘의 코디",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body className={jua.className}>
-        <RecoilProvider>
-          <GetProfile />
-          <header className="flex h-[75px] items-center shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
-            <div className="container flex w-full items-center justify-between">
-              <Link
-                href={"/"}
-                className="relative flex items-center gap-2 text-3xl text-orange"
-              >
-                <Image
-                  src="/logo.svg"
-                  alt="Next.js Logo"
-                  width={50}
-                  height={50}
-                  priority
-                />
-                DailyC
-              </Link>
-              <ul className="flex gap-8 text-2xl">
-                <Link href={"/write"}>
-                  <li>글쓰기</li>
-                </Link>
-                <Link href={"/profilepage"}>
-                  <li>마이페이지</li>
-                </Link>
-                <LoginAndLogoutBtn />
-              </ul>
-            </div>
-          </header>
-          {children}
-        </RecoilProvider>
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en">
+			<body className={jua.className}>
+				<RecoilProvider>
+					<GetProfile />
+					<header className="flex h-[75px] items-center shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
+						<div className="container flex w-full items-center justify-between">
+							<Link href={"/"} className="relative flex items-center gap-2 text-3xl text-orange">
+								<Image src="/logo.svg" alt="Next.js Logo" width={50} height={50} priority />
+								DailyC
+							</Link>
+							<ul className="flex gap-8 text-2xl">
+								<Link href={"/write"}>
+									<li>글쓰기</li>
+								</Link>
+								<Link href={"/profilepage"}>
+									<li>마이페이지</li>
+								</Link>
+								<LoginAndLogoutBtn />
+							</ul>
+						</div>
+					</header>
+					{children}
+					<footer className="h-24 flex flex-col items-center justify-center bg-gray-200 text-gray-600">
+						<div>@DAILY C</div>
+						<div>대표 : 서지훈 | 윤창근 | 윤호준 | 임세현</div>
+					</footer>
+				</RecoilProvider>
+			</body>
+		</html>
+	);
 }
