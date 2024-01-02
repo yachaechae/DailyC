e1546e89ce8e739e75dffd12d76d7e59e5de0920
volant97 <volant97@naver.com>
2024-01-02 15:09:56 +0900

    🐛 recoil 초기값을 아예 처음부터 유저정보 세팅하여 해결

--- src/recoil/state.ts
+++ src/recoil/state.ts
@@ -1,18 +1,38 @@
+import { getUser } from "@/utils/auth";
 import { atom } from "recoil";
+
+let isLoginFetch: boolean = false;
+let userFetch = {
+  id: "",
+  email: "",
+  nickname: "",
+  height: "",
+  gender: "",
+  userImg: "",
+};
+
+const value = localStorage.getItem("sb-kpgqztxpkosfevreubvj-auth-token");
+const localStorageData = value ? JSON.parse(value) : null;
+const localUserData = localStorageData?.user;
+
+if (!!localStorageData) {
+  isLoginFetch = true;
+  userFetch = {
+    id: localUserData.id,
+    email: localUserData.email,
+    nickname: localUserData.user_metadata.nickname,
+    height: localUserData.user_metadata.height,
+    gender: localUserData.user_metadata.gender,
+    userImg: localUserData.user_metadata.userImg,
+  };
+}
 
 export const isLoginState = atom({
   key: "isLoginState",
-  default: false,
+  default: isLoginFetch,
 });
 
 export const userState = atom({
   key: "userState",
-  default: {
-    id: "",
-    email: "",
-    nickname: "",
-    height: "",
-    gender: "",
-    userImg: "",
-  },
+  default: userFetch,
 });
