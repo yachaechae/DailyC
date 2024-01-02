257deffb45de1a002d7601a7315c7835ead73a44
volant97 <volant97@naver.com>
2024-01-02 14:35:03 +0900

    🐛 logout 시 user 정보 삭제

--- src/components/main/LoginAndLogoutBtn.tsx
+++ src/components/main/LoginAndLogoutBtn.tsx
@@ -1,20 +1,28 @@
 "use client";
 import React from "react";
 import { useRouter } from "next/navigation";
-import { getUser, logout } from "@/utils/auth";
-import { useEffect } from "react";
+import { logout } from "@/utils/auth";
 import Link from "next/link";
 import { useRecoilState } from "recoil";
-import { isLoginState } from "@/recoil/state";
+import { isLoginState, userState } from "@/recoil/state";
 
 function LoginAndLogoutBtn() {
   const router = useRouter();
   const [isLogin, setIsLogin] = useRecoilState(isLoginState);
+  const [user, setUser] = useRecoilState(userState);
 
   const hendleLogout = () => {
     if (isLogin === false) return alert("로그인된 이메일이 없습니다.");
     logout();
     setIsLogin(false);
+    setUser({
+      id: "",
+      email: "",
+      nickname: "",
+      height: "",
+      gender: "",
+      userImg: "",
+    });
     alert(`로그아웃 되었습니다.
 로그인 페이지로 이동합니다.`);
     router.push("/login");
