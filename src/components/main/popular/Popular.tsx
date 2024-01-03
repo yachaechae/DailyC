68c65b5b4e9b1d6f68e424312f35caa9ce4f1a3e
volant97 <volant97@naver.com>
2024-01-03 12:15:42 +0900

    💄 오늘의 코디 텍스트 수정

--- src/components/main/popular/Popular.tsx
+++ src/components/main/popular/Popular.tsx
@@ -1,71 +1,76 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import BasicCard from "../card/BasicCard";
-import { ArrowBackIosRounded, ArrowForwardIosRounded } from "@mui/icons-material";
+import {
+  ArrowBackIosRounded,
+  ArrowForwardIosRounded,
+} from "@mui/icons-material";
 import { IconButton } from "@mui/material";
 import { getAllPosts } from "@/api/write";
 
 export default function Popular() {
-	const [postData, setPostData] = useState<postType[]>([]);
-	const [currentIndex, setCurrentIndex] = useState(0);
+  const [postData, setPostData] = useState<postType[]>([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-	useEffect(() => {
-		async function fetchData() {
-			try {
-				const data: postType[] = await getAllPosts();
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data: postType[] = await getAllPosts();
 
-				setPostData(data);
-			} catch (error) {
-				return (
-					<>
-						<div>게시글을 불러오던 중 에러가 발생했습니다</div>
-					</>
-				);
-			}
-		}
-		fetchData();
-	}, []);
-	useEffect(() => {
-		setCurrentIndex(0);
-	}, [postData]);
+        setPostData(data);
+      } catch (error) {
+        return (
+          <>
+            <div>게시글을 불러오던 중 에러가 발생했습니다</div>
+          </>
+        );
+      }
+    }
+    fetchData();
+  }, []);
+  useEffect(() => {
+    setCurrentIndex(0);
+  }, [postData]);
 
-	const onNext = () => {
-		setCurrentIndex((prevIndex) => (prevIndex + 1) % postData.length);
-	};
+  const onNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % postData.length);
+  };
 
-	const onPrev = () => {
-		setCurrentIndex((prevIndex) => (prevIndex - 1 + postData.length) % postData.length);
-	};
+  const onPrev = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + postData.length) % postData.length,
+    );
+  };
 
-	return (
-		<>
-			<h2 className="mb-14 text-4xl">인기 코디</h2>
+  return (
+    <>
+      <h2 className="mb-14 text-4xl">오늘의 코디</h2>
 
-			<div className="w-full flex justify-between items-center overflow-hidden">
-				<div className="h-[370px] flex items-center z-10 bg-white ">
-					<IconButton onClick={onPrev} className="z-10 ">
-						<ArrowBackIosRounded sx={{ fontSize: 40 }} color="primary" />
-					</IconButton>
-				</div>
-				<div
-					className="w-full flex overflow-hidden"
-					style={{
-						transform: `translateX(${-currentIndex * 25}%)`,
-						transition: "transform 0.5s ease-in-out",
-					}}
-				>
-					{postData.map((post, index) => (
-						<div key={index} className="flex-none" style={{ width: "25%" }}>
-							<BasicCard data={post} />
-						</div>
-					))}
-				</div>
-				<div className="h-[370px] flex items-center z-10 bg-white ">
-					<IconButton onClick={onNext}>
-						<ArrowForwardIosRounded sx={{ fontSize: 40 }} color="primary" />
-					</IconButton>
-				</div>
-			</div>
-		</>
-	);
+      <div className="flex w-full items-center justify-between overflow-hidden">
+        <div className="z-10 flex h-[370px] items-center bg-white ">
+          <IconButton onClick={onPrev} className="z-10 ">
+            <ArrowBackIosRounded sx={{ fontSize: 40 }} color="primary" />
+          </IconButton>
+        </div>
+        <div
+          className="flex w-full overflow-hidden"
+          style={{
+            transform: `translateX(${-currentIndex * 25}%)`,
+            transition: "transform 0.5s ease-in-out",
+          }}
+        >
+          {postData.map((post, index) => (
+            <div key={index} className="flex-none" style={{ width: "25%" }}>
+              <BasicCard data={post} />
+            </div>
+          ))}
+        </div>
+        <div className="z-10 flex h-[370px] items-center bg-white ">
+          <IconButton onClick={onNext}>
+            <ArrowForwardIosRounded sx={{ fontSize: 40 }} color="primary" />
+          </IconButton>
+        </div>
+      </div>
+    </>
+  );
 }
